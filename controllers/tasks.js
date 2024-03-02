1704cc37e40707c7b844133edc7599fe81ce984d
pratikshanaik12 <41194108+pratikshanaik12@users.noreply.github.com>
2024-03-02 18:34:25 -0500

    initial push

---
+++ controllers/tasks.js
@@ -0,0 +1,70 @@
+const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
+const getAllTasks = asyncWrapper(async (req, res) =>{
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+})
+
+const createTask = async (req, res) =>{
+    try{
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+
+    } catch(e){
+        res.status(500).json({msg: e})
+    }
+
+    
+}
+
+const getTask = async (req, res) =>{
+    try{
+        const {id: taskID} = req.params
+        const task = await Task.findOne({_id: taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch(e){
+        res.status(500).json({msg: e})
+    }
+}
+
+const updateTask = async (req, res) =>{
+    try{
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body,{
+            new: true,
+            runValidators: true
+        })
+        if (!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch(e){
+        res.status(500).json({msg: e})
+    }
+}
+
+const deleteTask = async (req, res) =>{
+    try{
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({_id: taskID})
+        if (!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+        
+
+    }catch(e){
+        res.status(500).json({msg: e})
+    }
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
